fd07e11e6f71bb79f01b01ee04d76f2b36a82ede
Ryan Culpepper <ryanc@racket-lang.org>
2020-03-12 14:44:09 +0100

    ...

--- main.rkt
+++ main.rkt
@@ -59,25 +59,6 @@
                             (postsrc-path src) (postsrc-path prev-src)))]
              [else (hash-set! seen (postsrc-name src) src)])))))
 
-;; build/cache-post : postsrc -> Void
-(define (build/cache-post src)
-  (match-define (postsrc path name cachedir) src)
-  (define src-timestamp (file-or-directory-modify-seconds path))
-  (define cache-timestamp (get-cache-timestamp cachedir))
-  (when #f
-    (eprintf "cache ~s ~a src ~s\n"
-             cache-timestamp
-             (if (< cache-timestamp src-timestamp) '< '>)
-             src-timestamp))
-  ;; FIXME: what about scribble file that depends on modified lib?
-  (unless (> cache-timestamp src-timestamp)
-    (make-directory* cachedir)
-    (build-post path cachedir)))
-
-(define (get-cache-timestamp cachedir)
-  (with-handlers ([exn:fail:filesystem? (lambda (e) -inf.0)])
-    (with-input-from-file (build-path cachedir "_cache.rktd") read)))
-
 ;; ============================================================
 ;; Indexes
 
